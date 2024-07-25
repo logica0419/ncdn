3a2329a2e1891ac01db74d344e83fb6a76cbba3d
logica0419 <84759675+logica0419@users.noreply.github.com>
2024-07-26 05:50:03 +0900

    adapt to wsl env

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,7 +4,14 @@ sudo apt update
 sudo apt install --no-install-recommends -y \
     libssl-dev sshpass dnsutils ethtool \
     supervisor iputils-ping tcpdump bind9-dnsutils \
-    build-essential libbpf-dev clang llvm
-sudo apt install linux-headers-$(uname -r)
+    build-essential libbpf-dev clang llvm \
+    flex bison dwarves libelf-dev bc
+
+git clone https://github.com/microsoft/WSL2-Linux-Kernel \
+  -b linux-msft-wsl-$(uname -r | cut -d"-" -f 1) --single-branch --depth=1
+cd WSL2-Linux-Kernel
+echo 3 | make -j$(nproc) KCONFIG_CONFIG=./Microsoft/config-wsl
+cd ..
+rm -rf WSL2-Linux-Kernel
 
 go install github.com/cespare/reflex@latest
